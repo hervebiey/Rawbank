a32b939c38d21b7345afca4a1736e19262a3c864
Hervé Biey <herve.biey@gmail.com>
2023-04-23 00:38:35 +0200

    Initial commit

---
+++ src/main/java/com/mazabu/rawbank/RawbankApplication.java
@@ -0,0 +1,13 @@
+package com.mazabu.rawbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RawbankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RawbankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mazabu/rawbank/RawbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mazabu.rawbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RawbankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
